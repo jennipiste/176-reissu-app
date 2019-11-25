01a9a60c9b62692652ccb21c5af77a78599b28f3
Jenni Pajukoski <jenni@giosg.com>
2019-11-25 20:23:29 +0200

    Implemented scrollable background

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,15 +1,22 @@
 import React, { useState,  useEffect } from 'react';
-import { StyleSheet, Text, View, Button, Image, TouchableNativeFeedback, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TouchableNativeFeedback, ScrollView, LayoutChangeEvent, NativeSyntheticEvent, NativeScrollEvent } from 'react-native';
 import { useNavigation } from 'react-navigation-hooks';
 
 import moment from 'moment';
 import { destinations, START_TIME } from  '../constants';
 
+const SCROLLABLE_CONTENT_HEIGHT = 3000;
 
 export const HomeScreen: React.FC = () => {
 
     const [ isLoading, setIsLoading ] = useState<boolean>(true);
     const [ timeUntil, setTimeUntil ] =  useState<{days: number, hours: number, minutes: number, seconds: number}>(undefined);
+
+    // States for background position
+    const [ scrollViewWidth, setScrollViewWidth ] = useState<number>(undefined);
+    const [ scrollViewHeight, setScrollViewHeight ] = useState<number>(undefined);
+    const [ progress, setProgress ] = useState<number>(undefined);
+    const [ bgPos, setBgPos ] = useState(undefined);
 
     const { navigate } = useNavigation();
 
@@ -21,6 +28,42 @@ export const HomeScreen: React.FC = () => {
         setIsLoading(false);
     });
 
+    useEffect(() => {
+        if (scrollViewWidth && scrollViewHeight) {
+            updatePositions();
+        }
+    }, [progress]);
+
+    const updatePositions = () => {
+        const bgWidth = scrollViewWidth;
+        const bgHeight = bgWidth * 5.8;
+        const bgTop = (progress * (bgHeight - scrollViewHeight)) + scrollViewHeight - bgHeight;
+        const bgPos = {width: bgWidth, height: bgHeight, bottom: bgTop, left: 0};
+        setBgPos(bgPos);
+        console.log("scrollViewHeight", scrollViewHeight);
+        console.log("bgHeight", bgHeight);
+        console.log("updatedBgPos", bgPos);
+    };
+
+    const onLayout = (event: LayoutChangeEvent) => {
+        const { x, y, width, height } = event.nativeEvent.layout;
+        console.log("onLayout", x, y, width, height);
+
+        setScrollViewWidth(width);
+        setScrollViewHeight(height);
+        setProgress(0);
+    };
+
+    const onScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) =>  {
+        const scrollY = event.nativeEvent.contentOffset.y;
+        const contentHeight = event.nativeEvent.contentSize.height;
+        const viewHeight = event.nativeEvent.layoutMeasurement.height;
+        const maxY = contentHeight - viewHeight;
+        const progress = Math.max(Math.min(scrollY / maxY, 1), 0);
+        console.log("progress", progress);
+        setProgress(progress);
+    };
+
     const onUsersPress = () => {
         navigate('Users');
     };
@@ -30,10 +73,14 @@ export const HomeScreen: React.FC = () => {
     };
 
     return (
-        <View>
+        <View onLayout={(event) => onLayout(event)} style={styles.view}>
             {isLoading
                 ? <View><Image source={require('../../assets/kitten.jpeg')} /></View>
-                : <ImageBackground source={require('../../assets/vietnam_placeholder.jpg')} style={styles.backgroundImage}>
+                : <>
+                    <Image
+                        source={require('../../assets/vietnam.jpeg')}
+                        style={[styles.background, bgPos]}
+                    />
                     {(timeUntil && timeUntil.days < 0  || timeUntil.hours < 0 || timeUntil.minutes < 0 || timeUntil.seconds < 0)
                         ? <View style={styles.timeUntilBackground}>
                             <View style={styles.timeUntilContainer}>
@@ -58,24 +105,29 @@ export const HomeScreen: React.FC = () => {
                                 </View>
                             </View>
                         </View>
-                        : <View style={styles.view}>
-                            {destinations.map((destination, index) =>
-                                <TouchableNativeFeedback
-                                    background={TouchableNativeFeedback.SelectableBackground()}
-                                    onPress={() => onDestinationPress(index)}
-                                    key={index}
+                        : <ScrollView
+                            style={styles.scrollView}
+                            onScroll={(event) => onScroll(event)}
+                        >
+                            <View style={{height: SCROLLABLE_CONTENT_HEIGHT}}>
+                                {destinations.map((destination, index) =>
+                                    <TouchableNativeFeedback
+                                        background={TouchableNativeFeedback.SelectableBackground()}
+                                        onPress={() => onDestinationPress(index)}
+                                        key={index}
                                     >
-                                    <View style={styles.date}>
-                                        <Text>{`${destination.name}`}</Text>
-                                    </View>
-                                </TouchableNativeFeedback>
-                            )}
-                            <View style={styles.button}>
-                                <Button title="Käyttäjät" onPress={() => onUsersPress()}/>
+                                        <View style={[styles.date, { position: 'absolute', top: (index + 1) * 550}]}>
+                                            <Text>{`${destination.name}`}</Text>
+                                        </View>
+                                    </TouchableNativeFeedback>
+                                )}
+                                <View style={styles.button}>
+                                    <Button title="Käyttäjät" onPress={() => onUsersPress()}/>
+                                </View>
                             </View>
-                        </View>
+                        </ScrollView>
                     }
-                </ImageBackground>
+                </>
             }
         </View>
     );
@@ -83,21 +135,24 @@ export const HomeScreen: React.FC = () => {
 
 const styles = StyleSheet.create({
     view: {
-        paddingVertical: 30,
-        paddingHorizontal: 20,
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    scrollView: {
+        flex: 1,
+    },
+    background: {
+        position: 'absolute',
+        zIndex: -1,
     },
     button: {
-        marginTop: 10,
+        marginTop: 30,
         marginBottom: 10,
     },
     date: {
         borderWidth: 1,
         padding: 10,
         margin: 5,
-    },
-    backgroundImage: {
-        width: '100%',
-        height: '100%',
     },
     timeUntilBackground: {
         alignItems: 'center',
