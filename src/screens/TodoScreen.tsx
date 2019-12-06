2d681cee5df066a8f0a8545432027f22371e98fe
Jenni Pajukoski <jenni@giosg.com>
2019-12-06 12:33:09 +0200

    Todo screen styling

--- src/screens/TodoScreen.tsx
+++ src/screens/TodoScreen.tsx
@@ -1,23 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import { useSafeArea } from 'react-native-safe-area-context';
 import { Text, View, TouchableWithoutFeedback, StyleSheet, ScrollView } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 import { Packing, Category } from '../interfaces';
 import firebase from 'firebase';
 
 export const TodoScreen: React.FC = () => {
 
     const [ packings, setPackings ] = useState<Packing[]>([]);
+    const [ totalItems, setTotalItems ] = useState<number>(0);
+    const [ checkedItems, setCheckedItems ] = useState<number>(0);
 
     const userUid = firebase.auth().currentUser.uid;
+
+    const insets = useSafeArea();
 
     const updatePackings = async () => {
         firebase.database().ref(`packings/${userUid}`).on('value', (snapshot) => {
             const result = snapshot.val();
             if (result) {
-                const packingList = Object.keys(result).map(key => {
+                const packingList: Packing[] = Object.keys(result).map(key => {
                     return result[key];
                 });
                 setPackings(packingList);
+                setTotalItems(packingList.length);
+                setCheckedItems(packingList.filter(packing => packing.completed).length);
             }
         });
     };
@@ -35,33 +42,64 @@ export const TodoScreen: React.FC = () => {
     };
 
     return (
-        <ScrollView style={styles.view}>
-            <Text style={styles.title}>Ota mukaan</Text>
-            {Object.values(Category).map((category, index) => {
-                const filteredPackings = packings.filter(packing => packing.category === category);
-                return <React.Fragment key={index}>
-                    <Text style={styles.category}>{category}</Text>
-                    {filteredPackings.map(item => {
-                        return <TouchableWithoutFeedback
-                            key={item.id}
-                            onPress={() => toggleItem(item.id)}
-                        >
-                            <View style={styles.itemView}>
-                                <FontAwesome style={styles.icon} name={item.completed ===  true ? 'check-square-o' : 'square-o'} />
-                                <Text>{item.name}</Text>
-                            </View>
-                        </TouchableWithoutFeedback>;
+        <View style={{
+            flex: 1,
+            marginTop: insets.top,
+            marginBottom: insets.bottom,
+            backgroundColor: '#F1F3FD',
+        }}>
+            <View style={styles.header}>
+                <Text style={styles.title}>Pakkaa mukaan</Text>
+                <Text style={styles.title}>{`${checkedItems}/${totalItems}`}</Text>
+            </View>
+            <View style={styles.view}>
+                <ScrollView style={styles.scrollView}>
+                    {Object.values(Category).map((category, index) => {
+                        const filteredPackings = packings.filter(packing => packing.category === category);
+                        return <React.Fragment key={index}>
+                            <Text style={styles.category}>{category}</Text>
+                            {filteredPackings.map(item => {
+                                return <TouchableWithoutFeedback
+                                    key={item.id}
+                                    onPress={() => toggleItem(item.id)}
+                                >
+                                    <View style={styles.itemView}>
+                                        <Ionicons style={{
+                                            ...styles.icon,
+                                            color: item.completed ===  true ? '#7800F9' : 'lightgray',
+                                            fontSize: item.completed ===  true ? 24 : 28,
+                                        }}
+                                            name={item.completed ===  true ? 'ios-checkbox' : 'ios-square-outline'}
+                                        />
+                                        <Text>{item.name}</Text>
+                                    </View>
+                                </TouchableWithoutFeedback>;
+                            })}
+                        </React.Fragment>;
                     })}
-                </React.Fragment>;
-            })}
-        </ScrollView>
+                </ScrollView>
+            </View>
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 30,
+    },
     view: {
-        paddingVertical: 30,
-        paddingHorizontal: 20,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        elevation: 5,
+        paddingHorizontal: 30,
+        paddingTop: 20,
+        backgroundColor: '#FFF',
+    },
+    scrollView: {
+        marginBottom: 100,
     },
     itemView: {
         display: 'flex',
@@ -69,20 +107,17 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         marginTop: 10,
         marginBottom: 10,
-        marginLeft: 20,
     },
     title: {
-        textAlign: 'center',
-        marginTop: 10,
         fontSize: 18,
+        fontWeight: 'bold',
     },
     category: {
         fontSize: 18,
-        textTransform: 'uppercase',
-        marginTop: 10,
+        fontWeight: 'bold',
+        marginVertical: 20,
     },
     icon: {
         marginRight: 20,
-        fontSize: 20,
     }
 });
