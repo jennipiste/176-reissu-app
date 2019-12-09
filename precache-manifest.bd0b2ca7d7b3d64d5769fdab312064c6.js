a8ece1b7d48cca9283ff42df7567c23e97c6990d
Oskari Lehto <oskari.lehto@gmail.com>
2019-12-09 20:21:12 +0200

    Updates

---
+++ precache-manifest.bd0b2ca7d7b3d64d5769fdab312064c6.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e03a9a95b3985fb8d0d",
+    "url": "/static/js/app.5ff0675b.chunk.js"
+  },
+  {
+    "revision": "440be4b4ed4ec37d369a",
+    "url": "/static/js/runtime~app.82cccf13.js"
+  },
+  {
+    "revision": "2bc166b0a6233900f890",
+    "url": "/static/js/2.53998f29.chunk.js"
+  },
+  {
+    "revision": "9b7428a348821a6e0c150499c0ae0625",
+    "url": "/static/media/no_avatar.9b7428a3.png"
+  },
+  {
+    "revision": "6165c9d7a2e729ba57b23dd93add5366",
+    "url": "/static/media/back-icon-mask.6165c9d7.png"
+  },
+  {
+    "revision": "fae52a96e89a2be80cc577723acbd2fc",
+    "url": "/static/media/kitten.fae52a96.jpeg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "5c9967bd9293128e312a05c49a18d31e",
+    "url": "/static/media/home_bg.5c9967bd.png"
+  },
+  {
+    "revision": "1edd5017f455b09c608e175b1285c06d",
+    "url": "/static/media/date_icon.1edd5017.png"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "5e695e96a003a79f7f97060bf49409a9",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "46767a63305ba290e4405d3cd18073c0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "279e51c135da5b306b7d01ee8a47827a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b460a9bb76b08d0e02560573c1220b3e",
+    "url": "/static/js/runtime~app.82cccf13.js.gz"
+  },
+  {
+    "revision": "bd56db0844d50841ee6e0b69eb5e794d",
+    "url": "/expo-service-worker.js.gz"
+  },
+  {
+    "revision": "46adbfbaa615daeda9c1de56f1a4c8fc",
+    "url": "/static/js/app.5ff0675b.chunk.js.gz"
+  },
+  {
+    "revision": "e13e7b5ecfb884f3e1dcfa69f37347f9",
+    "url": "/static/js/2.53998f29.chunk.js.gz"
+  }
+];
